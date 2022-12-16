05390ae7ed2480fb839dec8946d64be7c7fc23cd
LaurenDarrimon <laurendarrimon@gmail.com>
2022-12-16 15:32:46 -0800

    add ISR revalidation to getStaticProps

--- pages/[id].js
+++ pages/[id].js
@@ -13,11 +13,11 @@ export async function getStaticProps({ params }) {
 
     const personData = await getData(params.id);
 
-
     return {
         props: {
             personData
-        }
+        },
+        revalidate: 60
     };
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import Layout from '../components/layout.js';
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Layout from "../components/layout.js";
 
-import { getOrderedList } from '../lib/data';
-import styles from '../styles/Home.module.css'
+import { getOrderedList } from "../lib/data";
+import styles from "../styles/Home.module.css";
 
 export async function getStaticProps() {
   const allPeople = await getOrderedList();
-  return{
+  return {
     props: {
-      allPeople
-    }
-  }
+      allPeople,
+    },
+    revalidate: 60
+  };
 }
 
 export default function Home({ allPeople }) {
   return (
     <Layout home>
-      <h1 className={styles.title}>
-        Sort of a Wizard Related Blog 
-      </h1>
+      <h1 className={styles.title}>Ollivander&apos;s Fine Wands</h1>
 
       <p className={styles.description}>
-        Mischief managed using a Next.js framework with PHP and SQL to query a WordPress Database
+        Mischief managed with Next.js and React using PHP and SQL to query a
+        WordpressDB
       </p>
 
       <div className="list-group">
-        {allPeople.map(({ id, name}) => (
-          <Link  key={id} href={`/${id}`}>
-            <a className="list-group-item list-group-item-action"> {name} </a> 
+        {allPeople.map(({ id, name }) => (
+          <Link key={id} href={`/${id}`}>
+            <a className="list-group-item list-group-item-action"> {name} </a>
           </Link>
         ))}
       </div>
     </Layout>
-  )
+  );
 }
